cca51b60903e8ab08a87c2b40570bfcc25026eb7
AmexHusky <johannes@fameitel.de>
2025-09-24 11:32:07 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+# Startet die Husky-Animation
+
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+FRAME_DIR="$(dirname "$0")/husky_frames"
+FRAME_FILES=("$FRAME_DIR/frame_"*.txt)
+
+ROUNDS=5
+SPEED=0.2
+
+for ((r=1; r<=ROUNDS; r++)); do
+    for frame_file in "${FRAME_FILES[@]}"; do
+        clear
+        echo -e "${BLUE}$(cat "$frame_file")${NC}"
+        sleep $SPEED
+    done
+done
+
+echo -e "${BLUE}Created by AmexHusky! 🐾${NC}"
 
